5b31d747c9d5d5cb79520e0219441ed17d64e3ad
Monika kumari <65735010+monika-4oop@users.noreply.github.com>
2022-06-26 16:58:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import {Page} from './components/page'
+
+function App() {
+  return (
+    <div className="App">
+      <Page/>
+
+    </div>
+  );
+}
+
+export default App;
